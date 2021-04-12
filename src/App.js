39ceeed71f656b2f70fd733a432143436c9d63cf
Ainne Oum <ainneo@gmail.com>
2021-04-12 00:45:39 -0700

    add custom hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Items from "./Items";
+import Item from "./components/Item";
+import useList from "./hooks/useList";
+
+const initList = [
+  { name: "tomato", calorie: 20 },
+  { name: "rice", calorie: 30 },
+  { name: "candy", calorie: 100 },
+];
 
 function App() {
+  const items = useList(initList);
+  const [editable, setEditable] = useState(false);
+
+  function removeItemHandle(e) {
+    items.removeItem(e.target.name);
+  }
+  function makeEditableHandle() {
+    setEditable(true);
+  }
+  function keyPressHandle(e, i) {
+    if (e.key === "Enter") {
+      setEditable(!editable);
+      items.saveItem(i, e.target.value);
+    }
+  }
+
   return (
     <div className="App">
-      <Items />
+      <header className="App-header">
+        <h2>Grocery List</h2>
+        {items.list.map((v, k) => {
+          return (
+            <Item
+              key={`${k}${v.name}${v.calorie}`}
+              item={v}
+              onClick={removeItemHandle}
+              editable={editable}
+              onDoubleClick={makeEditableHandle}
+              onKeyPress={keyPressHandle}
+              index={k}
+            />
+          );
+        })}
+      </header>
     </div>
   );
 }
